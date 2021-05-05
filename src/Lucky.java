87f7019b2e8ac54705c2cc5f3373a8723dff9cc3
yulkino <yulkino@hotmail.com>
2021-05-05 12:21:38 +0500

    Task11.1

--- src/Lucky.java
+++ src/Lucky.java
@@ -1,27 +1,23 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Lucky {
-    static volatile int x = 0;
-    static int count = 0;
-    private static Object lock = new Object();
+    static AtomicInteger AtomicX = new AtomicInteger(0);
+    static AtomicInteger count = new AtomicInteger(0);
 
     static class LuckyThread extends Thread {
         @Override
         public void run() {
-                counting(999999);
-        }
-
-        public void counting(long c){
-            synchronized (lock){
-                while (x < c) {
-                    x++;
-                    if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 ==
-                            (x / 1000) % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
-                        System.out.println(x);
-                        count++;
-                    }
+            int x;
+            while ((x = AtomicX.incrementAndGet()) < 1000000) {
+                if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 ==
+                        (x / 1000) % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
+                    System.out.println(x);
+                    count.incrementAndGet();
                 }
             }
         }
     }
+
 
     public static void main(String[] args) throws InterruptedException {
         Thread t1 = new LuckyThread();
